3928fe52033df7b4783499d35dd7409d2e63ca0d
Anwesh Mandaloju <49284273+anweshannu@users.noreply.github.com>
2021-11-07 17:44:29 +0530

    Update post_shell_script.sh

--- post_shell_script.sh
+++ post_shell_script.sh
@@ -3,8 +3,11 @@ ls -la
 # Creates a docker image using Dockerfile
 docker build -t tomcat-image .
 
+if [[ echo $( $(docker ps -aq --filter "name=tomcat-deploy"))]];
+then
 #Deletes the existing docker container with name 'tomcat-deploy'
 docker rm -f $(docker ps -aq --filter "name=tomcat-deploy")
+fi
 
 # Creates and runs the container using tomcat-image
 docker run -itd --name tomcat-deploy -p 80:8080 tomcat-image
